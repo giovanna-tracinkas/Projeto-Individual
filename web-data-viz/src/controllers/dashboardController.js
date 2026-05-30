ff813f391c8f3c83057d65ad3a8017b576797e68
giovanna-tracinkas <giovanna.tmenesio@sptech.school>
2026-05-30 02:27:50 -0300

    feat: parametrizando controller do gráfico de álbuns

--- web-data-viz/src/controllers/dashboardController.js
+++ web-data-viz/src/controllers/dashboardController.js
@@ -1,10 +1,18 @@
 var dashboardModel = require("../models/dashboardModel");
 
 function enviarQuiz(req, res) {
-    var fkAlbum = req.body.fkAlbumServer;
+    var nomeAlbum = req.body.nomeAlbumServer;
     var fkUsuario = req.body.fkUsuarioServer;
+    var perfil = req.body.perfilServer;
 
-    dashboardModel.enviarQuiz(fkAlbum, fkUsuario)
+    var sempiternal = req.body.sempiternalServer;
+    var amo = req.body.amoServer;
+    var survivalHorror = req.body.survivalHorrorServer;
+    var nexGen = req.body.nexGenServer;
+    var thereIsAHell = req.body.thereIsAHellServer;
+    var thatsTheSpirit = req.body.thatsTheSpiritServer;
+
+    dashboardModel.enviarQuiz(nomeAlbum, fkUsuario, perfil, sempiternal, amo, survivalHorror, nexGen, thereIsAHell, thatsTheSpirit)
         .then(
             function (resultado) {
                 res.json(resultado);
@@ -19,6 +27,7 @@ function enviarQuiz(req, res) {
                 res.status(500).json(erro.sqlMessage);
             }
         );
+        console.log("CHEGOU NO CONTROLLER");
 }
 
 function buscarAlbum(req, res) {
@@ -41,7 +50,27 @@ function buscarAlbum(req, res) {
         );
 }
 
+function compatibilidadeAlbum(req, res) {
+    var fkUsuario = req.params.fkUsuario;
+    dashboardModel.compatibilidadeAlbum(fkUsuario)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao buscar dados do gráfico no banco! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
 module.exports = {
     enviarQuiz,
-    buscarAlbum
+    buscarAlbum,
+    compatibilidadeAlbum
 }
